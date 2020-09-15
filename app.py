c6c6eece8dc42f73a5b39f1528b8343b67f78593
unknown <branchi468@gmail.com>
2020-09-15 07:46:48 -0400

    Actuialización

--- app.py
+++ app.py
@@ -4,59 +4,80 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 from openpyxl import Workbook
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def prueba():
+	wb = Workbook()
+	ruta = './static/centros.xlsx'
+	hoja = wb.active
+	hoja.title = "Números centros dentales"
 	centros = []
 	numeros = []
 	driver = webdriver.Chrome()
 	driver.get("http://regcess.mscbs.es/regcessWeb/inicioBuscarCentrosAction.do")
 	indice = driver.find_elements_by_css_selector("#tipoCentroId > option")
-	a = 0
-	b = 0
-	for i in indice:
-		b = 0
-		a = a + 1
-		selector = driver.find_element_by_css_selector("#tipoCentroId > option:nth-child(" + str(a) + ")")
-		datos = ActionChains(driver).click(selector).perform()
-		try:
-			WebDriverWait(driver, 3).until(EC.alert_is_present(),
-                                   'Timed out waiting for PA creation ' +
-                                   'confirmation popup to appear.')
-			alert = driver.switch_to.alert
-			alert.accept()
-		except TimeoutException:
-			datos1 = driver.find_element_by_css_selector("body > form > div.formLayout > div.formFoot > input")
-			indice2 = driver.find_elements_by_css_selector("body > div.tableContainer > table > tbody > tr")
-			datos2 = ActionChains(driver).click(datos1).perform()
-			for x in indice2:
-				b = b + 1
+	a = 1
+	terminado = 0
+	selector = driver.find_element_by_css_selector("#tipoCentroId > option:nth-child(11)")
+	datos = ActionChains(driver).click(selector).perform()
+	datos1 = driver.find_element_by_css_selector("body > form > div.formLayout > div.formFoot > input")
+	datos2 = ActionChains(driver).click(datos1).perform()
+	indice2 = driver.find_elements_by_css_selector("body > div.tableContainer > table > tbody > tr")
+	if terminado == 0:
+		def datos(b):
+			for b in range(2, len(indice2) + 1):
 				datos3 = driver.find_element_by_css_selector("body > div.tableContainer > table > tbody > tr:nth-child(" + str(b) + ") > td:nth-child(9) > a")
 				datos4 = ActionChains(driver).click(datos3).perform()
+				b = b + 1
 				datos5 = driver.find_element_by_css_selector("body > div.tableContainer > div:nth-child(7) > div.caja3 > div.campoSeccionDetalleCentro").text
 				datos6 = driver.find_element_by_css_selector("body > div.tableContainer > div:nth-child(5) > div.caja1 > div.campoSeccionDetalleCentro").text
+				print(datos6, datos5)
 				numeros.append(datos5)
 				centros.append(datos6)
+				i = len(numeros)
+				hoja['A' + str(i)] = datos6
+				hoja['B' + str(i)] = datos5
+				wb.save(filename = ruta)
 				datos7 = driver.find_element_by_css_selector("body > div.tableContainer > div.tableHead > div > form > input")
 				datos8 = ActionChains(driver).click(datos7).perform()
-			datos9 = driver.find_element_by_css_selector("body > div.tableContainer > div.tableHead > div > button")
-			datos10 = ActionChains(driver).click(datos9).perform()
+
+				if b == len(indice2):
+					try:
+						datas = driver.find_element_by_xpath("/html/body/div[2]/div[4]/a[1]")
+
+						if datas.text == "siguiente>>":
+							datosxs = ActionChains(driver).click(datas).perform()
+							datos(2)
+
+					except NoSuchElementException:
+
+						try:
+
+							datas2 = driver.find_element_by_xpath("/html/body/div[2]/div[4]/a[3]")
+
+							if datas2.text == "siguiente>>":
+
+								datosxs = ActionChains(driver).click(datas2).perform()
+								datos(2)
+
+							else:
+								terminado = 1
+								break
+						except NoSuchElementException:
+							terminado = 1
+							break
+			datos(2)
 	driver.close()
-	data = {centros[0]: numeros[0]}
-	wb = Workbook()
-	ruta = './static/centros.xlsx'
-	hoja = wb.active
-	hoja.title = "Números centros"
-	fila = 1 
-	col_fecha = 1 
-	col_dato = 2
-	for dato in data.items():
-	    hoja.cell(column=col_fecha, row=fila, value=dato[0])
-	    hoja.cell(column=col_dato, row=fila, value=dato[1])
-	fila+=1
+
+	'''for i in range(0, len(numeros) + 1):
+
+		hoja['A' + str(i)] = centros[i]
+		hoja['B' + str(i)] = numeros[i]'''
+
 	wb.save(filename = ruta)
 	print(centros, numeros)
 	return jsonify(centros, numeros)
